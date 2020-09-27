11dcdb56ff12cd0b4c9b49916256bb975a75e31e
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-09-27 12:08:48 +0100

    login and auth added

--- pages/forms.py
+++ pages/forms.py
@@ -17,4 +17,4 @@ class ContactForm(forms.Form):
   
 class LoginForm(forms.Form):
   username = forms.CharField()
-  password = forms.CharField()
+  password = forms.CharField(widget=forms.PasswordInput)
--- pages/views.py
+++ pages/views.py
@@ -1,17 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ContactForm, LoginForm
+from django.contrib.auth import authenticate, login
 
 def home_page(req):
   return render(req,'home_page.html',{})
 
+
+
+
 def about_page(req):
   return render(req,'about_page.html',{})
 
+
+
+
+
 def login_page(req):
-  return render(req,'auth/login.html',{})
+  login_form = LoginForm(req.POST or None)
+  print(req.user.is_authenticated)
+  # validate form
+  if login_form.is_valid():
+    # check if user is valid
+    username = req.POST.get('username')
+    password = req.POST.get('password')
+    user = authenticate(req, username=username, password=password)
+    if user is not None:
+      # success - login user and redirect
+      login(req,user)
+      print(req.user.is_authenticated)
+      return redirect('/login')
+    else:
+      print('WRONG CREDENTIALS')
+    
+  context = {
+    'form' : login_form
+  }
+  return render(req,'auth/login.html',context)
 
 def register_page(req):
   return render(req,'auth/register.html',{})
+
+
 
 
 
