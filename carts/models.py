cde2b649262374db52a723fc9f403689fd895af8
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-01 10:44:30 +0100

    cart create view added

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Cart(models.Model):
+  user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+  products = models.ManyToManyField(Product, blank=True)
+  total = models.DecimalField(decimal_places=2, max_digits=7, default=0.00)
+  updated = models.DateTimeField(auto_now=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  
+  # def __str__(self):
+  #   return (self.id)
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 def cart_home(req):
+  cart_id = req.session.get('cart_id', None)
+  if cart_id == None:
+    cart = Cart.objects.create(user=None)
+    req.session['cart_id'] = cart.id
+  else:
+    query = Cart.objects.filter(id=cart_id)
+    if query.count() == 1:
+        cart = query.first()
+    else:
+        cart = Cart.objects.create(user=None)
+    req.session['cart_id'] = cart.id
+  
   return render(req, 'carts/home.html', {})
