ad31bd2081e86eaa7bf428197728fc1d14dd3b3e
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-06 21:33:52 +0100

    product detail images template

--- watchlist/views.py
+++ watchlist/views.py
@@ -9,9 +9,12 @@ def update_watchlist(req, product_id):
   if not req.user.is_authenticated:
     return redirect('login')
   # get_or_create Watchlist by user
-  watchlist_obj = Watchlist.objects.get_or_create(user=req.user)
+  watchlist_obj,new_watchlist = Watchlist.objects.get_or_create(user=req.user)
   # get Product
-  # check if product is in Watchlist object
-  # toggle in the watchlist
-  print(product_id)
+  product_obj = Product.objects.get(id=product_id)
+  # toggle product in cart
+  if product_obj in watchlist_obj.products.all():
+    watchlist_obj.products.remove(product_obj)
+  else:
+    watchlist_obj.products.add(product_obj)
   return redirect('list')
