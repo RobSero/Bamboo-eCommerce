f408aa90c03469602acaea3f205f60023d569dbf
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-12 19:04:40 +0100

    deploy

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -1,5 +1,5 @@
 import os
-import django_heroku
+# import django_heroku
 # import dotenv
 # import environ
 # env = environ.Env(
@@ -10,14 +10,14 @@ import django_heroku
 # environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['.herokuapp.com']
 SECRET_KEY = '6-zp6=o@y!2of_vevx-w25)&^804z$gqwk8m%j1&3_#$6&vvhr'
 DEBUG = True
 # Application definition
@@ -126,7 +126,7 @@ USE_TZ = True
 
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_local")
 ]
---
+++ ecommerce/settings/production.py
@@ -0,0 +1,154 @@
+import os
+# import django_heroku
+# import dotenv
+# import environ
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
+# # reading .env file
+# environ.Env.read_env()
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['.herokuapp.com']
+SECRET_KEY = '6-zp6=o@y!2of_vevx-w25)&^804z$gqwk8m%j1&3_#$6&vvhr'
+DEBUG = True
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'storages',
+    'pages',
+    'products',
+    'search',
+    'tags',
+    'carts',
+    'order',
+    'accounts',
+    'watchlist',
+    'address'
+]
+
+# change default user model to custom model
+AUTH_USER_MODEL = 'accounts.User'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ecommerce.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bamboo',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LOGOUT_REDIRECT_URL = 'login'
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+
+
+# django_heroku.settings(locals())
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_local")
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_main", 'static_root')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_main", 'media_root')
+
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+from ecommerce.aws.conf import *
+
+
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
