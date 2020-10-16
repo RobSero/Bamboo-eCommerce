2c898ecede04e32324dcbff40d54fb5a311d5605
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-16 12:41:10 +0100

    stripe payment integration

--- payments/views.py
+++ payments/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+import stripe
 
+User = get_user_model()
 # Create your views here.
 def payment_page(req):
   return render(req,'carts/paymentpage.html', {})
 
 
 def charge_card(req):
+  user = req.user
   if req.method == 'POST':
     print(req.POST)
+    print(user.full_name)
+    customer = stripe.Customer.create(
+      name= user.full_name,
+      email = user.email,
+      source = req.POST['stripeToken']
+    )
+    charge = stripe.Charge.create(
+      customer=customer,
+      amount=5000,
+      currency='gbp',
+      description='bamboo payment'
+    )
   return render(req,'carts/paymentpage.html', {})
