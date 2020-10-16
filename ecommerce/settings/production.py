d60151e7263e6979767fcd1969918dd143387634
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-16 15:53:30 +0100

    stripe and payment page finished, polishing required

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -1,13 +1,8 @@
 import os
 import django_heroku
-# import dotenv
-# import environ
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False)
-# )
-# # reading .env file
-# environ.Env.read_env()
+import stripe
+
+stripe.api_key = os.environ.get('STRIPE_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- payments/views.py
+++ payments/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+
+
+
+from carts.models import Cart
+from accounts.forms import LoginForm
+from address.forms import AddressForm
+from address.models import Address
+from order.models import Order
+from products.models import Product
 import stripe
-
-
 
 User = get_user_model()
 # Create your views here.
 def payment_page(req):
-  return render(req,'carts/paymentpage.html', {})
+  cart, new_cart = Cart.objects.new_cart_or_get(req)
+  if new_cart or cart.products.count() == 0:
+    return redirect('carthome')
+  order, new_order = Order.objects.get_or_create(cart=cart)
+  user = req.user
+  login_form = LoginForm()
+  user_address, new_address = Address.objects.get_or_create(user=user)
+  address_form = AddressForm(instance=user_address)
+  context = {
+    'object' : order,
+    'cart' : cart,
+    'login_form' : login_form,
+    'address_form' : address_form
+  }
+  return render(req,'carts/paymentpage.html', context)
 
 
 def charge_card(req):
