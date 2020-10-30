5afe825bfd0cfeb74abe2dbaf12f2c58b5f7446c
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-10-30 16:04:16 +0000

    testing products and user auth apps

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 
 # Create your tests here.
+client = Client()
+User = get_user_model()
+
+
+class URLGetRequests(TestCase):
+  
+  def test_register_template(self):
+    register_res = client.get('/account/register/')
+    self.assertEqual(register_res.status_code, 200)
+    self.assertTemplateUsed(register_res, 'auth/register.html')
+    
+  
+    def test_login_template(self):
+      login_res = client.get('/account/login/')
+      self.assertEqual(login_res.status_code, 200)
+      self.assertTemplateUsed(login_res, 'auth/login.html')
+      
+      
+class UserTest(TestCase):
+  def setUp(self):
+    new_user = User.objects.create_user(email='test@email.com',password='test123', full_name='John Tester')
+    
+  def test_get_user(self):
+    test_user = User.objects.get(email='test@email.com')
+    self.assertEqual(test_user.full_name,'John Tester')
+  
+  def test_login_user_and_session(self):
+      login_res = client.post('/account/login/', {'email': 'test@email.com', 'password':'test123'})
+      self.assertEqual(login_res.status_code, 200)
+    
+  
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductTestSuite(TestCase):
+  def setUp(self):
+    Product.objects.create(title='testProduct', slug='sluggage', description='nice new test products for sale', price=3.25)
+    Product.objects.create(title='Piri Piri Banana', slug='piri', description='tasty potassium', price=2.00)
+    Product.objects.create(title='Apples', slug='yummypiri', description='tasty piri cyanide', price=2.00)
+    
+    
+  def test_get_product(self):
+    new_product = Product.objects.get(title='testProduct')
+    self.assertEqual(new_product.price, 3.25)
+    self.assertEqual(new_product.slug, 'sluggage')
+    
+  def test_search_queries(self):
+    empty_search_product_queryset = Product.objects.search('chicken')
+    filled_product_queryset = Product.objects.search('piri')
+    empty_featured_product_queryset = Product.objects.feature()
+    all_products = Product.objects.all()
+    
+    self.assertFalse(empty_search_product_queryset)
+    self.assertTrue(filled_product_queryset)
+    self.assertEqual(filled_product_queryset.first().title, 'Piri Piri Banana')
+    self.assertEqual(filled_product_queryset.last().title, 'Apples')
+    self.assertFalse(empty_featured_product_queryset)
+    self.assertEqual(len(all_products), 3)
+  
+
+
